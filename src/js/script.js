2017cf0c72846cf55b89a58029cf02c348b9afe6
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2024-12-11 16:25:18 +0100

    landing page

--- src/js/script.js
+++ src/js/script.js
@@ -14,3 +14,19 @@ if (toggle && nav) {
     page.classList.toggle("noscroll", isClosed);
   });
 }
+const carousel = document.querySelector(".carousel__container");
+const nextButon = document.querySelector(".carousel__button--next");
+const prevButon = document.querySelector(".carousel__button--prev");
+const premierItem = document.querySelector(".carousel__item");
+
+const scrollAmount = premierItem.clientWidth;
+
+if (carousel) {
+   prevButon.addEventListener("click", ()=>{
+    carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" })
+   }) 
+
+    nextButon.addEventListener("click", ()=>{
+     carousel.scrollBy({ left: +scrollAmount, behavior: "smooth" })
+    }) 
+ };
